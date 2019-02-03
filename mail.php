2fa8b60a55866160a000626ba4be4e20b6f96c6d
Andrew William Chumchal <awchumchal@chumchal.net>
2019-02-02 18:04:18 -0600

    Updated 020219

--- mail.php
+++ mail.php
@@ -2,14 +2,15 @@
 
 	$to = "website@aandechumchal.com"; // this is your Email address
 	$from  = $_POST['email']; // this is the sender's Email address
-	$sender_name = $_POST['name'];
+	$sender_first_name = $_POST['fname'];
+	$sender_first_name = $_POST['lname'];
 	$number_of_gustes = $_POST['guest'];
 	$events = $_POST['events'];
 	$notes = $_POST['notes'];
 
 
 	$subject = "Form submission";
-	$message = $sender_name . " is attending! The number of gustes of his / her is : " .  $number_of_gustes . " and his / her selected event is " . $events . ". He / she worte the following... ". "\n\n" . $notes;
+	$message = $sender_first_name . $sender_first_name . " is attending! The number of gustes of his / her is : " .  $number_of_gustes . " and his / her selected event is " . $events . ". He / she worte the following... ". "\n\n" . $notes;
 
 	$headers = 'From: ' . $from;
 	mail($to, $subject, $message, $headers);
