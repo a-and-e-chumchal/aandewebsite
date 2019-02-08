8cc0cf7e21a803b16df60284e1a4c50245d677c4
Andrew William Chumchal <awchumchal@chumchal.net>
2019-02-07 19:43:19 -0600

    020718

--- mail.php
+++ mail.php
@@ -3,14 +3,14 @@
 	$to = "website@aandechumchal.com"; // this is your Email address
 	$from  = $_POST['email']; // this is the sender's Email address
 	$sender_first_name = $_POST['fname'];
-	$sender_first_name = $_POST['lname'];
+	$sender_last_name = $_POST['lname'];
 	$number_of_gustes = $_POST['guest'];
 	$events = $_POST['events'];
 	$notes = $_POST['notes'];
 
 
-	$subject = "Form submission";
-	$message = $sender_first_name . $sender_first_name . " is attending! The number of gustes of his / her is : " .  $number_of_gustes . " and his / her selected event is " . $events . ". He / she worte the following... ". "\n\n" . $notes;
+	$subject = $sender_first_name . "Form submission";
+	$message = $sender_first_name . " " . $sender_last_name . " is attending!" . "\n\n" . "* Number of guest of is : " .  $number_of_gustes . "\n\n" . "* Selected event is " . $events . "\n\n" . "* Wrote the following... ". "\n\n" . $notes;
 
 	$headers = 'From: ' . $from;
 	mail($to, $subject, $message, $headers);
